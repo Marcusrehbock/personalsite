c06abb80178b7c1bea3bb0bffb9812917808d9cf
Marcus <marcusrehbock@gmail.com>
2023-11-08 14:24:41 +0100

    3

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+// postcss.config.js
+export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-  }
-}
+    // Any other PostCSS plugins you want to use
+  },
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,21 @@
 <section class="py-8 z-10 font-serif">
-    <div class="flex flex-col md:flex-row items-center max-w-6xl px-6 py-8 mx-auto">
-      <div class="w-full md:w-1/2 py-8">
-        <h1 class="text-purple-900 text-7xl font-semibold leading-none tracking-tighter">
-          Welcome to <br><span class="text-blue-500">My Portfolio, <br></span> I am Web Developer.
-        </h1>
-      </div>
-      <div class="w-full md:w-1/2 py-8">
-        <img src="https://www.svgrepo.com/show/493509/person-who-invests.svg" class="g-image">
-      </div>
+  <div class="flex flex-col md:flex-row items-center max-w-6xl px-6 py-8 mx-auto space-y-8 md:space-y-0">
+    <div class="w-full md:w-1/2 py-8">
+      <h1 class="text-purple-900 text-7xl font-semibold leading-none tracking-tighter mb-4">
+        Welcome to <br><span class="text-blue-500">My Portfolio</span><br> I am a Web Developer.
+      </h1>
+      <form class="mt-8">
+        <label for="email" class="block text-sm font-medium text-gray-700">Stay updated with my latest work</label>
+        <div class="mt-1 flex rounded-md shadow-sm">
+          <input type="email" name="email" id="email" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-l-md sm:text-sm border-gray-300" placeholder="Enter your email">
+          <button type="submit" class="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-r-md text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+            Subscribe
+          </button>
+        </div>
+      </form>
     </div>
-  </section>
-  
+    <div class="w-full md:w-1/2 py-8">
+      <img src="https://www.svgrepo.com/show/493509/person-who-invests.svg" alt="Person Who Invests" class="g-image">
+    </div>
+  </div>
+</section>
